9ab6e4f027580a306e93b1d29caf4ebdbc765ab8
Mehdi AZZABI <mehdi.azzabi@outlook.com>
2025-06-05 11:26:08 +0200

    First commit : Init app with spring initilizer

---
+++ src/main/java/com/mazzabi/KataFOOBARQUIX/KataFoobarquixApplication.java
@@ -0,0 +1,13 @@
+package com.mazzabi.KataFOOBARQUIX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataFoobarquixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataFoobarquixApplication.class, args);
+	}
+
+}
